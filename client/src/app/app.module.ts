b7ad03239f0592ab5007938377c038f1972f6ccd
Natalia <natorgom@gmail.com>
2019-02-27 09:36:33 +0100

    add routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RobotVacuumComponent } from './robot-vacuum/robot-vacuum.component';
+import { Top10Component } from './top10/top10.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DashboardComponent,
-    RobotVacuumComponent
+    RobotVacuumComponent,
+    Top10Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
