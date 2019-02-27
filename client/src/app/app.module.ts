0a02b00a73ee80005be7eb6392337b4959e365dc
Natalia <natorgom@gmail.com>
2019-02-27 09:14:23 +0100

    Create navbar, dashboard and robotVacuum components. First css on navBar done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RobotVacuumComponent } from './robot-vacuum/robot-vacuum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    RobotVacuumComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/robot-vacuum/robot-vacuum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-robot-vacuum',
+  templateUrl: './robot-vacuum.component.html',
+  styleUrls: ['./robot-vacuum.component.css']
+})
+export class RobotVacuumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
